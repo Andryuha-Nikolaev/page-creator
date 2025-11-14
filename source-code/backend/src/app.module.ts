3f768da7427f2462d59e8196ec785ab89fa8653c
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-11-14 09:21:07 +0300

    #add #backend - validation errors pipe

--- source-code/backend/src/app.module.ts
+++ source-code/backend/src/app.module.ts
@@ -1,9 +1,34 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  UnprocessableEntityException,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [ConfigModule.forRoot(), UserModule, AuthModule],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        exceptionFactory: (errors) => {
+          return new UnprocessableEntityException({
+            statusCode: 422,
+            error: 'Unprocessable Entity',
+            errors: errors.reduce(
+              (acc, e) => ({
+                ...acc,
+                [e.property]: Object.values(e.constraints ?? ''),
+              }),
+              {},
+            ),
+          });
+        },
+      }),
+    },
+  ],
 })
 export class AppModule {}
