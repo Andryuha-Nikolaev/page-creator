1c1947d42553a1aa1a08a1e2eff2eab9ad7d3dba
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-11-17 15:43:28 +0300

    #add #backend - transform string values

--- source-code/backend/src/user/user.dto.ts
+++ source-code/backend/src/user/user.dto.ts
@@ -1,18 +1,26 @@
 import { ApiProperty, ApiPropertyOptional, OmitType } from '@nestjs/swagger';
 import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
 import { User } from 'src/_gen/prisma-class/user';
+import {
+  ToLowerCaseString,
+  TrimString,
+} from 'src/common/decorators/transform-value.decorator';
 
 export class UserDto {
+  @TrimString()
   @IsEmail()
   @IsOptional()
+  @ToLowerCaseString()
   @ApiPropertyOptional()
   email?: string;
 
+  @TrimString()
   @IsString()
   @IsOptional()
   @ApiPropertyOptional()
   name?: string;
 
+  @TrimString()
   @IsOptional()
   @MinLength(6, {
     message: 'Password must be at least 6 characters long',
