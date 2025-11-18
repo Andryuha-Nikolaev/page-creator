7b976456a0aa3adc4da61b2dbe0945db63fd37a9
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-11-18 14:14:02 +0300

    #add #admin - auth

--- source-code/backend/src/admin/admin.module.ts
+++ source-code/backend/src/admin/admin.module.ts
@@ -3,6 +3,18 @@ import AdminJS, { ResourceWithOptions } from 'adminjs';
 import { PrismaService } from 'src/prisma.service';
 import { dark } from '@adminjs/themes';
 
+const DEFAULT_ADMIN = {
+  email: process.env.ADMIN_EMAIL,
+  password: process.env.ADMIN_PASSWORD,
+};
+
+const authenticate = async (email: string, password: string) => {
+  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
+    return Promise.resolve(DEFAULT_ADMIN);
+  }
+  return null;
+};
+
 AdminJS.registerAdapter({ Database, Resource });
 
 export default import('@adminjs/nestjs').then(({ AdminModule }) =>
@@ -44,6 +56,16 @@ export default import('@adminjs/nestjs').then(({ AdminModule }) =>
           branding: {
             companyName: 'PageCreator Admin',
           },
+        },
+        auth: {
+          authenticate,
+          cookieName: 'adminjs',
+          cookiePassword: 'secret',
+        },
+        sessionOptions: {
+          resave: true,
+          saveUninitialized: true,
+          secret: 'secret',
         },
       };
     },
