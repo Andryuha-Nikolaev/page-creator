5d298fb3dcb152c55bee449e58d7468186b0d3d9
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-11-18 08:58:02 +0300

    #add #admin - init adminjs prisma

--- source-code/backend/src/admin/admin.module.ts
+++ source-code/backend/src/admin/admin.module.ts
@@ -1,10 +1,26 @@
+import { Database, Resource, getModelByName } from '@adminjs/prisma';
+import AdminJS from 'adminjs';
+import { PrismaService } from 'src/prisma.service';
+
+AdminJS.registerAdapter({ Database, Resource });
+
 export default import('@adminjs/nestjs').then(({ AdminModule }) =>
   AdminModule.createAdminAsync({
-    useFactory: () => ({
-      adminJsOptions: {
-        rootPath: '/admin',
-        resources: [],
-      },
-    }),
+    useFactory: () => {
+      const prisma = new PrismaService();
+
+      return {
+        adminJsOptions: {
+          rootPath: '/admin',
+          resources: [
+            {
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+              resource: { model: getModelByName('User'), client: prisma },
+              options: {},
+            },
+          ],
+        },
+      };
+    },
   }),
 );
