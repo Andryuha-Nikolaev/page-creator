77428fb33e3fa0db2318ad3b4a0df88eefc248d7
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-11-22 20:50:40 +0300

    #delete #frontend(code gen) - unused code

--- source-code/frontend/src/app/page.tsx
+++ source-code/frontend/src/app/page.tsx
@@ -1,15 +1,5 @@
-import { login } from "$shared/api/code-gen";
 import { HomePage } from "$pages/home";
 
-export default async function Home() {
-	const res = await login({
-		body: {
-			email: "andryuha_nikolaev@mail.ru",
-			password: "123456",
-		},
-	});
-
-	console.log(res.response.ok);
-
+export default function Home() {
 	return <HomePage />;
 }
