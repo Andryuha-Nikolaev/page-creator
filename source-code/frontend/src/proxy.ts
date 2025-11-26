f2c5bd151511adcd414c199a476d317c0082f1c5
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-11-26 09:48:48 +0300

    #add #frontend(auth) - no auth redirect

--- source-code/frontend/src/proxy.ts
+++ source-code/frontend/src/proxy.ts
@@ -3,11 +3,20 @@ import { NextResponse, type NextRequest } from "next/server";
 import { parse } from "set-cookie-parser";
 
 import { getNewTokens } from "$shared/api/code-gen";
+import { ACCOUNT_ROUTE } from "$shared/config";
 import { AUTH_CONSTANTS } from "$app/develop/auth/config/constants";
 
 export async function proxy(request: NextRequest) {
 	const accessToken = request.cookies.get(AUTH_CONSTANTS.ACCESS_TOKEN_NAME);
 	const refreshToken = request.cookies.get(AUTH_CONSTANTS.REFRESH_TOKEN_NAME);
+
+	if (!accessToken && !refreshToken) {
+		if (request.nextUrl.pathname.startsWith(ACCOUNT_ROUTE)) {
+			const response = NextResponse.redirect(new URL(request.nextUrl.origin));
+
+			return response;
+		}
+	}
 
 	if (!accessToken && refreshToken) {
 		try {
