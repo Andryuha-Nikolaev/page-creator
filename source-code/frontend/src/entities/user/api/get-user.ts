fd0486e4330a6b99fe2f5403861435f9c003ac10
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-11-29 19:22:34 +0300

    #rafactor #frontend(auth)

--- source-code/frontend/src/entities/user/api/get-user.ts
+++ source-code/frontend/src/entities/user/api/get-user.ts
@@ -1,9 +1,15 @@
 "use server";
 
+import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 import { profile } from "$shared/api/code-gen";
-import { REVALIDATE_TAGS, ROUTES_CONSTANTS } from "$shared/config";
+import {
+	ACCOUNT_ROUTE,
+	HEADERS,
+	REVALIDATE_TAGS,
+	ROUTES_CONSTANTS,
+} from "$shared/config";
 import { getHeadersFromCookies } from "$shared/lib";
 
 import { isUserMaybeAuthorized } from "../lib/checks";
@@ -11,7 +17,13 @@ import { isUserMaybeAuthorized } from "../lib/checks";
 export async function getUser() {
 	const isMaybeAuthorized = await isUserMaybeAuthorized();
 
+	const headersList = await headers();
+	const pathname = headersList.get(HEADERS.PATHNAME);
+
 	if (!isMaybeAuthorized) {
+		if (pathname?.startsWith(ACCOUNT_ROUTE)) {
+			redirect(ROUTES_CONSTANTS.LOGOUT);
+		}
 		return null;
 	}
 
